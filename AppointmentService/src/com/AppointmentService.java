2ff9a4aaaf4ff98490220d094c2764649c831aca
ChamendiPabasara <53684409+ChamendiPabasara@users.noreply.github.com>
2020-05-03 13:52:52 +0530

    Implement GET AppointmentService

--- AppointmentService/src/com/AppointmentService.java
+++ AppointmentService/src/com/AppointmentService.java
@@ -1,5 +1,45 @@
 package com;
+import java.sql.Date;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+//For JSON
+import com.google.gson.*;
+
+import model.Appointment;
+
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+import model.Appointment;
+
+@Path("/Appointments")
 public class AppointmentService {
+	
+	Appointment App1 = new Appointment();
+
+	@GET
+	@Path("/")
+	@Produces(MediaType.TEXT_HTML)
+	public String ReadAppointments() {
+		
+		return App1.ReadAppointments();
+	}
+	
+	@GET
+	@Path("/getAppointmentbyID/App1/{id}")
+	@Produces(MediaType.TEXT_HTML)
+	public String getPayment(@PathParam("id") int id) {
+		System.out.println(id);
+
+		return this.App1.getAppointmentByPatient(id);
+	}	
+	
 
 }
